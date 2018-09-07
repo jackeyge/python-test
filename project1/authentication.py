6fa115a4bbbb55b3baaf72a188de7e0636b16d0b
jackeyge <gexyong@163.com>
2018-09-07 14:11:33 +0800

    test

---
+++ project1/authentication.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env pyhton
+'''
+This is test
+'''
+
+def login(username, password):
+	try:
+		user_file = open('./users.txt')
+		user_buf = user_file.read()
+		user = [line.split("|") for line in user_buf.split("\n")]
+		if [username, password] in users:
+			return True
+		else:
+			return False
+	except Exception, exc:
+		print "I can't authenticate you."
+		return False
+	
+def logout():
+	print 'test'
+	print 'test2'
---
+++ tests/authentication_tests.py
@@ -0,0 +1,20 @@
+﻿#!/usr/bin/env python
+'''
+Unit tests
+'''
+
+from unittest import TestCase
+from mock import patch
+import project1.authentication as auth
+
+class StandAloneTests(TestCase):
+	
+	@patch('__builtin__.open')
+	def test_login(self, mock_open):
+		"""正确的测试"""
+		mock_open.return_value.read.return_value = \
+		    "netease|password\n"
+		self.assertTrue(auth.login('netease', 'password'))
+	
+#	@patch('__builtin__.open')
+	
