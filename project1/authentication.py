3efe9e31fc39e2efc4812046df2c769bd0ee4e6c
jackeyge <gexyong@163.com>
2018-09-09 21:35:16 +0800

    test

--- project1/authentication.py
+++ project1/authentication.py
@@ -5,7 +5,7 @@ This is test
 
 def login(username, password):
 	try:
-		user_file = open('./users.txt')
+		user_file = open('users.txt')
 		user_buf = user_file.read()
 		user = [line.split("|") for line in user_buf.split("\n")]
 		if [username, password] in users:
