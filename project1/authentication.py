d0f5c229c711c84b600be29e2b5e90aaab230f87
jackeyge <gexyong@163.com>
2018-09-10 10:07:25 +0800

    s

--- project1/authentication.py
+++ project1/authentication.py
@@ -8,7 +8,7 @@ def login(username, password):
 		user_file = open('users.txt')
 		user_buf = user_file.read()
 		user = [line.split("|") for line in user_buf.split("\n")]
-		if [username, password] in users:
+		if [username, password] in user:
 			return True
 		else:
 			return False
@@ -19,3 +19,6 @@ def login(username, password):
 def logout():
 	print 'test'
 	print 'test2'
+	
+	
+login('netease', 'password')
